883721b2b55138fff6965d2970e3a6e29b708753
Mihir Prajapati <mihir1737@gmail.com>
2026-02-07 17:55:16 +0530

    Handle concurrent connections

--- app/main.go
+++ app/main.go
@@ -9,29 +9,12 @@ import (
 	"strings"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-	// Accepts the connection
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
+func handleRequest(conn net.Conn) {
 	// Always close your connections!
 	// Used defer means, it works after the function returns
 	// benifit of placing this statement here is in case of error as well
 	// the connection will be closed.
 	defer conn.Close()
-
 	// wrap the connection in a buffered reader
 	reader := bufio.NewReader(conn)
 
@@ -40,15 +23,14 @@ func main() {
 
 	if err != nil {
 		fmt.Println("Error parsing the request.", err)
+		return
 	}
-
 	// construct the row HTTP response string
 	// Each line end with \r\n (CRLF)
 
 	var response []byte
 
 	path := request.URL.Path
-
 	switch {
 	case path == "/index.html" || path == "/":
 		response = []byte("HTTP/1.1 200 OK\r\n\r\n")
@@ -77,5 +59,25 @@ func main() {
 	if err != nil {
 		fmt.Println("Error writing to connection: ", err.Error())
 	}
+}
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
 
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+
+	for {
+		// Accepts the connection
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleRequest(conn)
+	}
 }
